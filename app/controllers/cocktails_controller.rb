d8d8fcc722029f9a9c4d713881a619a8c9ded1cb
Steven Chern <stevenchern@gmail.com>
2019-08-22 16:08:11 -0300

    create controllers and methods for cocktail and doses

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,32 @@
+class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: :show
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show; end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.all.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+  resources :doses
+  resources :cocktails
 end
+
+# cocktails index show (ingredients and doses) new
+
+
+# doses new create destroy(ingredient and description to an existing cocktail)
+
+
