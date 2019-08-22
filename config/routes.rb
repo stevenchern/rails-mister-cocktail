b2c1bc0a4d2e22859e7e3847742733e5e4361e77
Steven Chern <stevenchern@gmail.com>
2019-08-22 17:37:37 -0300

    almost finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  resources :doses
-  resources :cocktails
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: :destroy
 end
-
 # cocktails index show (ingredients and doses) new
-
 
 # doses new create destroy(ingredient and description to an existing cocktail)
 
